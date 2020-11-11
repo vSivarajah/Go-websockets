56bb3b45f48ea9fd5a10cdabc19f305b0443a76b
Vignesh  Sivarajah <vigneshsivarajah@Vigneshs-MacBook-Pro.local>
2020-11-11 23:20:09 +0100

    app.sj

--- parcel_blueprint/src/Components/app.js
+++ parcel_blueprint/src/Components/app.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 
