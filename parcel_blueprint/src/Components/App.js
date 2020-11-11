b33c1f56ab5e7073f0df041355646f7e9cbfde7e
Vignesh  Sivarajah <vigneshsivarajah@Vigneshs-MacBook-Pro.local>
2020-11-12 00:18:05 +0100

    dfps

--- parcel_blueprint/src/Components/App.js
+++ parcel_blueprint/src/Components/App.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 
 const StyledApp = styled.div`
     border: 1px solid #f00;
-
 `;
 
 
