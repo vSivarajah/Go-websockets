53f027dc7b32f5c1c113cfd8566ce57daf051120
Vignesh  Sivarajah <vigneshsivarajah@Vigneshs-MacBook-Pro.local>
2020-11-12 09:51:14 +0100

    sdf

--- parcel_blueprint/src/Components/App.js
+++ parcel_blueprint/src/Components/App.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-import styled from 'styled-components';
+var styled = require('styled-components');
 
 const StyledApp = styled.div`
     border: 1px solid #f00;
